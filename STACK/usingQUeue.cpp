64fa46de1ea93ab55ab29fb084964eee807d0631
pawan0110 <meroy0075@gmail.com>
2025-09-02 07:29:24 +0530

    Implement code changes to enhance functionality and optimize performance

---
+++ STACK/usingQUeue.cpp
@@ -0,0 +1,40 @@
+#include <queue>
+using namespace std;
+
+
+//using single queue
+class MyStack
+{
+    queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x)
+    {
+        int s = q.size();
+        q.push(x);
+        for (int i = 0; i < s; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop()
+    {
+        int n = q.front();
+        q.pop();
+        return n;
+    }
+
+    int top()
+    {
+        return q.front();
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
