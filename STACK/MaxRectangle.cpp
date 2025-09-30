ca132caf69874c2031520edc3625a9bfc09856b4
pawan0110 <meroy0075@gmail.com>
2025-09-30 12:42:37 +0530

    implement max rectangle in histogram

---
+++ STACK/MaxRectangle.cpp
@@ -0,0 +1,21 @@
+// max rectangle in histogram
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+       int n = heights.size();
+       stack<int> st;
+       for(int i =0; i<n; i++){
+        int rect = 2*(min(heights[i],heights[i+1]));
+        while(!st.empty() && st.top() < rect){
+            st.pop();
+        }
+        st.push(rect);
+       } 
+       int ans = st.top();
+       return ans;
+    }
+};
+// tc = O(n*n)   tle;
+//sc = 0(n)
