a1d291a036205f9537d2aaeb06c4d34f7670aa2c
pawan0110 <meroy0075@gmail.com>
2025-10-16 19:44:19 +0530

    .

--- heap/minHeapImplementaion.cpp
+++ heap/minHeapImplementaion.cpp
@@ -1,83 +1,97 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution {
     vector<int> heap;
 
-    int parent(int i) {
-        return (i-1)/2;
-    }
-    int left(int i) {
-        return (2*i+1);
-    }
-    int right(int i) {
-        return (2*i+2);
-    }
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return 2 * i + 1; }
+    int right(int i) { return 2 * i + 2; }
 
-    void heapify(int i){
+    void heapify(int i) {
         int l = left(i);
         int r = right(i);
         int smallest = i;
 
-        if(l < heap.size() && heap[i]<heap[smallest]) smallest = l;
-        if(r < heap.size() && heap[r]<heap[smallest]) smallest = r;
-        if(smallest != i){
-            swap(heap[i],heap[smallest]);
+        if (l < heap.size() && heap[l] < heap[smallest])
+            smallest = l;
+
+        if (r < heap.size() && heap[r] < heap[smallest])
+            smallest = r;
+
+        if (smallest != i) {
+            swap(heap[i], heap[smallest]);
             heapify(smallest);
         }
     }
-    public:
 
-        void initializeHeap(){
-            heap.clear();
+public:
+    void initializeHeap() {
+        heap.clear();
+    }
+
+    void insert(int key) {
+        heap.push_back(key);
+        int i = heap.size() - 1;
+
+        // Fix the min-heap property
+        while (i != 0 && heap[parent(i)] > heap[i]) {
+            swap(heap[i], heap[parent(i)]);
+            i = parent(i);
         }
+    }
 
-        void insert(int key){
-            heap.push_back(key);
-            int i = heap.size()-1;
+    void changeKey(int index, int new_val) {
+        if (index < 0 || index >= heap.size())
+            return;
 
-            while(i != 0 && heap[parent(i)] >heap[i]) {
-                swap(heap[i],heap[parent(i)]);
-                i = parent(i);
-            }
-              
+        heap[index] = new_val;
+        // If decreased, bubble up
+        while (index != 0 && heap[parent(index)] > heap[index]) {
+            swap(heap[index], heap[parent(index)]);
+            index = parent(index);
         }
+        // If increased, fix down
+        heapify(index);
+    }
 
-        void changeKey(int index, int new_val){
-            if(index<0 || index>heap.size()) return ;
-            heap[index] = new_val;
-            while(index != 0 && heap[index]<heap[parent(index)]){
-                swap[heap[index], heap[parent(index)]];
-                index = parent(new_val);
-            }
-        }
+    int extractMin() {
+        if (heap.empty())
+            return INT_MAX;
 
-        void extractMin(){
-            if(heap.size()<= 0) return INT_MAX;
-            if(heap.size()==1) return [--size];
+        if (heap.size() == 1) {
             int root = heap[0];
-            heap[0] = heap[--size];
-            heapify(0);
+            heap.pop_back();
             return root;
-            
-        }
-        bool isEmpty() { 
-            return heap.empty();
         }
 
+        int root = heap[0];
+        heap[0] = heap.back();
+        heap.pop_back();
 
-        int getMin() {
-            if (heap.empty()) {
-              cout << "Heap is empty!\n";
-              return INT_MAX;
-            }
-            return heap[0]; 
+        heapify(0);
+        return root;
+    }
+
+    bool isEmpty() {
+        return heap.empty();
+    }
+
+    int getMin() {
+        if (heap.empty()) {
+            cout << "Heap is empty!\n";
+            return INT_MAX;
         }
+        return heap[0];
+    }
 
+    int heapSize() {
+        return heap.size();
+    }
 
-        int heapSize() {
-           return heap.size();
-        }
-
- 
+    void printHeap() {
+        for (int val : heap)
+            cout << val << " ";
+        cout << "\n";
+    }
 };
