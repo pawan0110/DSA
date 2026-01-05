5c9b36fd17052a2dde366e1a3b62ded567cb2ac2
pawan0110 <meroy0075@gmail.com>
2026-01-05 14:51:40 +0530

    feat: deteact cyclic grpah using dfs

--- graph/isCyclicGraph.cpp
+++ graph/isCyclicGraph.cpp
@@ -1,16 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool dfs(int node, int parent, vector<int>& visited, vector<int> adj[]) {
-    visited[node] = 1;
 
-    for (int neighbor : adj[node]) {
-        if (!visited[neighbor]) {
-            if (dfs(neighbor, node, visited, adj))
+class solution
+{
+public:
+    bool dfs(int node, int parent, vector<int> adj[], vector<int> &visited)
+    {
+        visited[node] = 1;
+
+        for (int neighbor : adj[node])
+        {
+            if (!visited[neighbor])
+            {
+                if (dfs(neighbor, node, adj, visited))
+                    return true;
+            }
+            else if (neighbor != parent)
+            {
                 return true;
+            }
         }
-        else if (neighbor != parent) {
-            return true;
-        }
+        return false;
     }
-    return false;
-}
+
+    bool isCycle(int v, vector<int> adj[])
+    {
+        vector<int> visited(v, 0);
+
+        for (int i = 0; i < v; i++)
+        {
+            if (!visited[i])
+            {
+                if (dfs(i, -1, adj, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
